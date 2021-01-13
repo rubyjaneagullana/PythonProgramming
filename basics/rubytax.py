e38370d10f28f55244ef9dcac4e62d2e5d27497d
Ruby Jane P. Agullana <77317140+rubyjaneagullana@users.noreply.github.com>
2021-01-13 13:29:21 +0530

    3

--- basics/rubytax.py
+++ basics/rubytax.py
@@ -1,12 +1,13 @@
-income=float(input("enter your  yearly salary: "))
-
+income=float(input("Enter your  yearly income:Rs. "))
 totaltax = 0
+
+
 if income > 0:
     taxable = min([income, 350000])
     income = income - taxable
-    print("taxable income=", taxable)
-    print("remaining income=", income)
-    totaltax=0
+    print("nontaxable income= Rs.", taxable)
+    print("taxable and remaining income=Rs.", income)
+    print("your tax for first Rs. 350,000 is:Rs. ",totaltax)
 
 
 
@@ -14,26 +15,36 @@ if income > 0:
 if income > 0:
     taxable = min([income,200000])
     income = income - taxable
-    print("taxable income=", taxable)
-    print("remaining income=", income)
-    totaltax= taxable*.10
-    print("total tax is :",totaltax)
+    print("taxable income @ 10%=Rs.", taxable)
+    print("remaining income=Rs.", income)
+    tax10= taxable*.10
+    print("Your tax @ 10% is:Rs. ",tax10)
+    totaltax=totaltax+tax10
+
 
 if income > 0:
     taxable = min([income,200000])
     income = income - taxable
-    print("taxable income=", taxable)
-    print("remaining income=", income)
-    totaltax =totaltax + (taxable * .20)
-    print("total tax is :",totaltax)
+    print("taxable income 20%=Rs.", taxable)
+    print("remaining income=Rs.", income)
+    tax20 =taxable * .20
+    print("Your tax @ 20% is :Rs.",tax20)
+    totaltax=totaltax+tax20
 
-if income > 200000:
+
+
+
+if income > 0:
     taxable = income
-    print("taxable income=", taxable)
-    tax3 = taxable * .30
-    print("tax at 30% is :",tax3)
-    totaltax = totaltax+tax3
-    print("total tax: ",totaltax)
+    print("taxable income 30%=Rs.", taxable)
+    print("remaining income=Rs.",taxable)
+    tax30 = taxable * .30
+    print("your tax @ 30% is :Rs.",tax30)
+    totaltax=totaltax+tax30
+
+
+print("Your total tax is Rs.",totaltax)
+
 
 
 
